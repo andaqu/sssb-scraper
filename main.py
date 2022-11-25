88c9615be8eb475cacd8841bf0d03cf8fc8ed099
wendru18 <wendru18@gmail.com>
2022-11-25 19:20:50 +0100

    filtering changes, sending new changes table, clickable addresses, and commenting

--- main.py
+++ main.py
@@ -15,33 +15,46 @@ apartments = []
 url = "https://sssb.se/en/looking-for-housing/apply-for-apartment/available-apartments/available-apartments-list/?paginationantal=100"
 driver.get(url)
 
+# Scrape apartment information (res) and links (res_links)
 res = driver.find_element(By.XPATH, "//*[@id='SubNavigationContentContainer']/div[3]/div[4]/div")
-
-df = pd.DataFrame(columns = ["Area", "Address", "Type", "Floor", "Living space", "Rent", "Moving date", "Days"])
-
-apartments = res.text.split("\n")
-rows = [apartments[i:i + 8] for i in range(0, len(apartments), 8)]
+res_links = driver.find_elements(By.XPATH, "//*[@id='SubNavigationContentContainer']/div[3]/div[4]/div/a")
 
 driver.close()
 
+# Build dataframe, populate the scraped data, and pre-process it
+df = pd.DataFrame(columns = ["Area", "Address", "Type", "Floor", "Living space", "Rent", "Moving date", "Days", "Link"])
+
+apartments = res.text.split("\n")
+rows = [apartments[i:i + 8] for i in range(0, len(apartments), 8)]
 df = df.append(pd.DataFrame(rows, columns=df.columns))
+
 df[["Days", "Students"]] = df["Days"].apply(lambda x: pd.Series(str(x).split(" (")))
 df["Students"] = df["Students"].apply(lambda x: pd.Series(x[:1]))
+df["Link"] = [e.get_attribute('href') for e in res_links]
 
-ideal_apartments = df[(df["Area"] == "Birka") | (df["Type"].str.startswith("2 rooms"))].sort_values(["Area", "Days"])
+# ~df["Area"].isin(["Flemingsberg", "Strix"])
+conditions_for_ideal =  (df["Area"] == "Birka") | (df["Type"] == "2 rooms & kitchen") | (df["Living space"].astype(int) >= 30)
+ideal_apartments = df[conditions_for_ideal].sort_values(["Area", "Days"])
 
 # The below code checks if the apartments were changed from the latest scrape. This is done by grabbing the previous scrapped "ideal apartments" and checking their address (i.e. building name & room number) with the newly scrapped ones.
 # If there was a change, the ideal ones are saved and sent to email.
 # Otherwise, nothing is done!
 
 previous_ideal = pd.read_csv("previous_ideal.csv")
-ideal_apartments = ideal_apartments.reset_index()
+ideal_apartments = ideal_apartments.reset_index(drop=True)
 
-compare = ideal_apartments["Address"].compare(previous_ideal["Address"])
+# From the previously identified ideal apartments, which are the new apartments?
+changes = ideal_apartments["Address"][~ideal_apartments["Address"].isin(previous_ideal["Address"])].drop_duplicates()
 
-if len(compare) != 0:
+if not changes.empty:
     print("There was an update in the listings!")
-    ideal_apartments.to_csv("previous_ideal.csv",index=False)
-    send_an_email(ideal_apartments)
+
+    # Populate the changes table with the needed information to comprehensively send it via email
+    changes = pd.merge(changes, ideal_apartments[["Address", "Type", "Area", "Link"]], on=["Address"])
+
+    # The link is only needed for email output, drop it before saving
+    ideal_apartments.drop(["Link"], axis=1).to_csv("previous_ideal.csv",index=False)
+    
+    send_an_email(ideal_apartments, changes)
 else:
     print("No update in listings!")
--- utils.py
+++ utils.py
@@ -3,13 +3,11 @@ from email.mime.text import MIMEText
 from decouple import config
 import smtplib
 
-EMAIL = config('EMAIL')
-PASS = config('PASS')
+EMAIL = config("EMAIL")
+PASS = config("PASS")
 
-def send_an_email(df):
-
-    df = df.drop(["index"], axis=1)
-    print(df)
+# This function should email two tables to EMAIL: the new posted ideal apartments on the SSSB website and the total ideal apartments (which most likely includes previously scraped ones)
+def send_an_email(ideal_apartments, changes):
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
@@ -18,17 +16,36 @@ def send_an_email(df):
 
     server.login(EMAIL, PASS)
 
-    subject = "Change in ideal apartment listing!"
+    # Change subject title according to primary interests; in this case, the primary interest is whether the new scraped apartments are stemming from Birka
+    if ((changes["Area"] == "Birka") & (changes["Type"] == "2 rooms & kitchen")).any():
+        subject = "New Birka 2-room apartment/s!"
+    elif (changes["Area"] == "Birka").any():
+        subject = "New Birka apartment/s!"
+    else:
+        subject = "New ideal apartments"
+
+    # Make clickable the Address parts of the tables
+    def make_clickable(url, name):
+        return f"<a href={url}>{name}</a>"
+
+    ideal_apartments["Address"] = ideal_apartments.apply(lambda x: make_clickable(x["Link"], x["Address"]), axis=1)
+    changes["Address"] = changes.apply(lambda x: make_clickable(x["Link"], x["Address"]), axis=1)
+    
+    # The link is only needed to make addresses clickable, drop them before sending email
+    ideal_apartments = ideal_apartments.drop(["Link"], axis=1)
+    changes = changes.drop(["Link"], axis=1)
+
     bodyText = ""
 
     bodyHTML = """\
     <html>
     <head></head>
-    <body>
-        {0}
-    </body>
+    <p> New scraped changes are as follows: <p>
+    {0}
+    <p> Total ideal apartments: <p>
+    {1}
     </html>
-    """.format(df.to_html())
+    """.format(changes.to_html(escape=False), ideal_apartments.to_html(escape=False))
 
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
@@ -46,3 +63,5 @@ def send_an_email(df):
         EMAIL,
         message.as_string()
     )
+
+    print("Email sent!")
